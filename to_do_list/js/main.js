21c195738aad2fbc416bccc3ca13330ffede0f06
Tyler Samuelson <tylercsamuelson@gmail.com>
2020-11-05 22:12:43 -0600

    Okay, this may not have been EXACTLY what the markdown called for, but it's a functional to-do list. Excited to refactor this using React.

--- to_do_list/js/main.js
+++ to_do_list/js/main.js
@@ -1,16 +1,40 @@
 // Cached DOM Nodes
-const form = document.getElementById("input-container");
+const form = document.querySelector("form");
 const input = document.querySelector("input");
-const button = document.querySelector("button");
-const ul = document.getElementById("to-do-items");
+const addButton = document.getElementById("add-button");
+const completeButton = document.getElementById("complete-button");
+const removeButton = document.getElementById("remove-button");
+const ul = document.getElementById("to-do-list");
+const li = document.getElementsByClassName("to-do-items");
 
+// Initialize userInput as an empty string
 let userInput = "";
+
+// Refactor your complex functions down into separate functions that are ideally
+// only doing a single, particular job...
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   userInput = input.value;
   let li = document.createElement("li");
+  li.classList.add("to-do-items");
   li.textContent = userInput;
+  let removeButton = document.createElement("button");
+  removeButton.innerText = "REMOVE";
+  removeButton.setAttribute("id", "remove-button");
+  removeButton.addEventListener("click", (e) => {
+    li.remove();
+    completeButton.remove();
+    e.target.remove();
+  });
+  let completeButton = document.createElement("button");
+  completeButton.innerText = "COMPLETE";
+  completeButton.setAttribute("id", "complete-button");
+  completeButton.addEventListener("click", (e) => {
+    li.classList.toggle("strike");
+  });
   ul.appendChild(li);
+  li.appendChild(completeButton);
+  li.appendChild(removeButton);
   input.value = "";
 });
